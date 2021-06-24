e6a28bbe900869096d795c864c2d5b7df29b0dbe
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-24 17:51:06 -0500

    Application code

---
+++ src/main/java/com/claudio/oauth2/resourceserver/photos/PhotosApplication.java
@@ -0,0 +1,15 @@
+package com.claudio.oauth2.resourceserver.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosApplication.class, args);
+	}
+
+}
